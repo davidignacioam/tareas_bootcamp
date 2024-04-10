f2436913cf022cdfb043dadd7191faaae064527d
David Ignacio <davidignacioam@gmail.com>
2024-04-10 17:22:04 -0400

    Update Ejercicio II – Window Functions.sql
    adding row_number sum rank solutions

--- Window Functions/Ejercicio II – Window Functions.sql
+++ Window Functions/Ejercicio II – Window Functions.sql
@@ -61,3 +61,100 @@ from products as p
 inner join categories as c on c.category_id = p.category_id 
 ;
 
+/* 6. ROW_NUMBER */
+select 
+	row_number() over (order by sum(od.quantity) desc) as Ránking,
+	p.product_name as Producto,
+	sum(od.quantity) as Total_Cantidad
+from products as p 
+left join categories as c on c.category_id = p.category_id 
+left join order_details as od on od.product_id  = p.product_id 
+group by p.product_name
+order by sum(od.quantity) desc
+;
+
+/* 7. ROW_NUMBER */
+select distinct
+	row_number() over (order by c.customer_id) as Ránking,
+	c.customer_id as ID_Cliente,
+	c.company_name as Compañía
+from customers as c
+order by row_number() over (order by c.customer_id) asc
+;
+
+/* 8. ROW_NUMBER */
+select 
+	row_number () over (order by e.birth_date desc) as Ránking,
+	concat(e.first_name,' ',e.last_name) as Nombre_Completo_Empleado,
+	e.birth_date as Fecha_Nacimiento
+from employees as e 
+;
+
+/* 9. SUM */
+select 
+	o.customer_id as ID_Cliente,
+	o.employee_id  as ID_Empleado,
+	o.order_id  as ID_Orden,
+	od.unit_price as Precio,
+	od.quantity  as Cantidad,
+	od.unit_price * od.quantity as Venta_Final,
+	sum(od.unit_price * od.quantity) over (partition by o.customer_id order by o.customer_id) as Suma_Total_Ventas_Clientes
+from orders as o
+inner join order_details as od on od.order_id  = o.order_id 
+;
+
+/* 10. SUM */
+select 
+	c.category_name as Categoría,
+	p.product_name as Producto,
+	od.unit_price as Precio,
+	od.quantity  as Cantidad,
+	od.unit_price * od.quantity as Venta_Final,
+	sum(od.unit_price * od.quantity) over (partition by c.category_name order by c.category_name) as Suma_Total_Ventas_Categorías
+from orders as o
+inner join order_details as od on od.order_id  = o.order_id 
+left join products as p on p.product_id = od.product_id 
+inner join categories as c on c.category_id = p.category_id 
+;
+
+/* 11. SUM */
+select 
+	o.ship_country as País, 
+	o.order_id as ID_Orden,
+	o.shipped_date as Fecha_Envío,
+	o.freight as Precio_Envío,
+	sum(o.freight) over (partition by o.ship_country) as Suma_Total_Precio_Envío
+from orders as o
+where o.shipped_date is not null
+order by o.ship_country, o.order_id asc
+;
+
+/* 12. RANK */
+select 
+	rank() over (order by sum(od.unit_price) desc) as Ránking,
+	o.customer_id as ID_Cliente,
+	sum(od.unit_price * od.quantity) as Suma_Total_Ventas
+from orders as o
+inner join order_details as od on od.order_id  = o.order_id 
+group by o.customer_id
+order by sum(od.unit_price) desc
+;
+
+/* 13. RANK */
+select 
+	e.employee_id as ID_Empleado,
+	concat(e.first_name,' ',e.last_name) as Nombre_Completo_Empleado,
+	e.hire_date as Fecha_Contratación,
+	rank () over (order by e.hire_date asc) as Ránking
+from employees as e 
+;
+
+/* 14. RANK */
+select 
+	p.product_id as ID_Producto,
+	p.product_name as Producto,
+	p.unit_price as Precio_Unitario,
+	rank () over (order by p.unit_price desc) as Ránking
+from products as p 
+order by p.unit_price desc
+;
